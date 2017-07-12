668c524163de4cd840a42db56daa573107f6eb9a
Nataliya <natasha.gapochka@gmail.com>
2017-07-12 08:13:26 -0700

    Update abstract-functions.js

--- abstract-functions.js
+++ abstract-functions.js
@@ -4,18 +4,34 @@
 var name = "Bob";
 var height = "72";
 var favoriteColor = "red";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
 
 var name = "Samantha";
 var height = "65";
 var favoriteColor = "orange";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
 
 var name = "Christopher";
 var height = "69";
 var favoriteColor = "green";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
 
 var name = "Tamera";
 var height = "63";
 var favoriteColor = "blue";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
 
 function personalIntroduction(name, height, favoriteColor){
   console.log("My name is " + name + ", and my height is " + height + ", and my favorite color is " + favoriteColor);
